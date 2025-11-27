1640879227553c94bfb688f123ea574a7ad993b3
Jerson Stiven Llumiquinga Moreno <jersonllumiquingam23@gmail.com>
2025-11-27 02:38:18 -0500

    up up up

---
+++ TAR1_P2_VELOCIMETRO/Velocimetro.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TAR1_P2_VELOCIMETRO
+{
+    partial class Velocimetro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxVelocimetro = new System.Windows.Forms.PictureBox();
+            this.timerVelocimetro = new System.Windows.Forms.Timer(this.components);
+            this.labelVelocidad = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVelocimetro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxVelocimetro
+            // 
+            this.pictureBoxVelocimetro.Location = new System.Drawing.Point(425, 25);
+            this.pictureBoxVelocimetro.Name = "pictureBoxVelocimetro";
+            this.pictureBoxVelocimetro.Size = new System.Drawing.Size(363, 397);
+            this.pictureBoxVelocimetro.TabIndex = 0;
+            this.pictureBoxVelocimetro.TabStop = false;
+            this.pictureBoxVelocimetro.Click += new System.EventHandler(this.pictureBoxVelocimetro_Click);
+            this.pictureBoxVelocimetro.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxVelocimetro_Paint);
+            // 
+            // labelVelocidad
+            // 
+            this.labelVelocidad.AutoSize = true;
+            this.labelVelocidad.Font = new System.Drawing.Font("Century Schoolbook", 66F, System.Drawing.FontStyle.Bold);
+            this.labelVelocidad.Location = new System.Drawing.Point(23, 201);
+            this.labelVelocidad.Name = "labelVelocidad";
+            this.labelVelocidad.Size = new System.Drawing.Size(95, 103);
+            this.labelVelocidad.TabIndex = 1;
+            this.labelVelocidad.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "VELOCIDAD (Km/h):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Schoolbook", 25.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "VELOCÍMETRO:";
+            // 
+            // Velocimetro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelVelocidad);
+            this.Controls.Add(this.pictureBoxVelocimetro);
+            this.Name = "Velocimetro";
+            this.Text = "Velocímetro";
+            this.Load += new System.EventHandler(this.Velocimetro_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Velocimetro_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVelocimetro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxVelocimetro;
+        private System.Windows.Forms.Timer timerVelocimetro;
+        private System.Windows.Forms.Label labelVelocidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TAR1_P2_VELOCIMETRO/Velocimetro.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace TAR1_P2_VELOCIMETRO
+{
+    public partial class Velocimetro : Form
+    {
+        private float velocidad = 0;  
+        private float anguloAguja = 0;
+        private const int radio = 150;
+        private const int centroX = 200;
+        private const int centroY = 200;
+
+        public Velocimetro()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;  
+            pictureBoxVelocimetro.Paint += pictureBoxVelocimetro_Paint;  
+            timerVelocimetro.Tick += timerVelocimetro_Tick;  
+            timerVelocimetro.Start();  
+        }
+
+        private void Velocimetro_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void timerVelocimetro_Tick(object sender, EventArgs e)
+        {
+        
+            anguloAguja = (velocidad / 180) * 180;  
+            pictureBoxVelocimetro.Invalidate();  
+        }
+
+        private void Velocimetro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (velocidad < 180)
+                {
+                    velocidad += 10; 
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (velocidad > 0)
+                {
+                    velocidad -= 10; 
+                }
+            }
+            labelVelocidad.Text = $"{velocidad} km/h";
+            if (velocidad > 120)
+            {
+                labelVelocidad.ForeColor = Color.Red;  
+            }
+            else
+            {
+                labelVelocidad.ForeColor = Color.Black;
+            }            
+            pictureBoxVelocimetro.Invalidate();
+        }
+
+
+        private void pictureBoxVelocimetro_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;            
+            Brush brushFondo = new LinearGradientBrush(
+                new Point(centroX, centroY),
+                new Point(centroX, centroY + radio),
+                Color.LightSkyBlue,
+                Color.White);
+            g.FillEllipse(brushFondo, centroX - radio, centroY - radio, radio * 2, radio * 2);            
+            Pen penBorde = new Pen(Color.DarkSlateGray, 6);
+            g.DrawEllipse(penBorde, centroX - radio, centroY - radio, radio * 2, radio * 2);            
+            for (int i = 0; i <= 180; i += 10)
+            {
+                float angulo = 180 - (i / 180.0f) * 180;
+
+                float x1 = centroX + radio * (float)Math.Cos(Math.PI * angulo / 180);
+                float y1 = centroY - radio * (float)Math.Sin(Math.PI * angulo / 180);
+                float x2 = centroX + (radio - 10) * (float)Math.Cos(Math.PI * angulo / 180);
+                float y2 = centroY - (radio - 10) * (float)Math.Sin(Math.PI * angulo / 180);
+                Pen penSecundaria = new Pen(Color.Gray, 2);
+                g.DrawLine(penSecundaria, x1, y1, x2, y2);
+                
+                if (i % 20 == 0)
+                {
+                    Pen penPrincipal = new Pen(Color.Blue, 3);
+                    g.DrawLine(penPrincipal, x1, y1, x2, y2);
+
+                    // Mostrar los números con contorno
+                    string texto = i.ToString();
+                    SizeF size = g.MeasureString(texto, this.Font);
+                    g.DrawString(texto, new Font("Arial", 10, FontStyle.Bold), Brushes.White, x2 - size.Width / 2 + 1, y2 - size.Height / 2 + 1);  // Sombra blanca
+                    g.DrawString(texto, new Font("Arial", 10, FontStyle.Bold), Brushes.Black, x2 - size.Width / 2, y2 - size.Height / 2);  // Texto principal
+                }
+            }
+
+            // Dibujar la aguja con borde y color más llamativo
+            Pen penAguja = new Pen(Color.Red, 4);
+            float anguloAgujaRad = (anguloAguja / 180) * (float)Math.PI;
+            float xAguja = centroX + (radio - 20) * (float)Math.Cos(Math.PI * (180 - anguloAguja) / 180);
+            float yAguja = centroY - (radio - 20) * (float)Math.Sin(Math.PI * (180 - anguloAguja) / 180);
+            g.DrawLine(penAguja, centroX, centroY, xAguja, yAguja);  // Aguja de velocidad
+        }
+
+        private void pictureBoxVelocimetro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelVelocidad_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
